14782c1d1912cdbabbe88252365c8d0c5a633dce
chengrui <1272946582@qq.com>
2024-03-26 09:57:39 +0800

    01 changed

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -15,9 +15,7 @@ public class Algorithms<T> {
       for (int i = 0; i < array.length - 1; i++) {
          for (int j = 0; j < array.length - 1 - i; j++) {
             if (array[j].compareTo(array[j + 1]) > 0) {
-               T tmp = array[j];
-               array[j] = array[j + 1];
-               array[j + 1] = tmp;
+               swap(array, j, j + 1);
             }
          }
       }
@@ -25,9 +23,7 @@ public class Algorithms<T> {
 
    public static <T> void reverse(T[] array) {
       for (int i = 0; i < array.length / 2; i++) {
-         T temp = array[i];
-         array[i] = array[array.length - i - 1];
-         array[array.length - i - 1] = temp;
+         swap(array, i, array.length - 1 - i);  
       }
    }
 
